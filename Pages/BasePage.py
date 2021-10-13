e8bf876030f41c8648cc8e7aca7a0864df1be7b6
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-10-13 16:11:00 +0530

    MojoGo POM V2 - logging and screenshots

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -1,3 +1,6 @@
+import inspect
+import logging
+
 import pytest
 
 
@@ -6,6 +9,20 @@ class BasePage:
 
     def implicitly_wait(self):
         self.driver.implicitly_wait(10)
+
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+
+        logger.addHandler(fileHandler)  # filehandler object
+
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+
 
 
 
